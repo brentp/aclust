b90b84cc06dd9fc8de6a21ac2a9aeed4e0d17d7a
Brent Pedersen <bpederse@gmail.com>
2014-08-25 17:50:33 -0600

    make example more flexible

--- examples/methylation-clustering-asthma.py
+++ examples/methylation-clustering-asthma.py
@@ -1,8 +1,8 @@
 """
-an example of how to use the output from aclust.
+An example of how to use the output from aclust.
 
 For each cluster, we get N probes. Since those will be correlated within
-an individuals, we use GEE's from a pull request on statsmodels.
+each individual, we use GEE's from a pull request on statsmodels.
 
 We could also use, for example, a mixed-effect model, e.g., in lme4 syntax:
 
@@ -14,66 +14,73 @@ a number of measurements equal to the number of probes in a given cluster.
 
 import sys
 from aclust import aclust
-from statsmodels.genmod.generalized_estimating_equations import GEE
-from statsmodels.genmod.dependence_structures.varstruct import (Exchangeable,
-        Independence, Autoregressive)
-from statsmodels.genmod.families import Gaussian, Binomial
-import statsmodels.api as sm
-Binomial = sm.families.Binomial
+from statsmodels.api import GEE, GLM
+from statsmodels.genmod.dependence_structures import Exchangeable
+from statsmodels.genmod.families import Gaussian
 import pandas as pd
 
+from scipy.stats import norm
+from numpy.linalg import cholesky as chol
+qnorm = norm.ppf
+pnorm = norm.cdf
 
 
-def gee_cluster(clust_iter, clin_df, model_str, coef, id_col,
-        varstruct=Exchangeable(), family=Gaussian()):
-    """
-    clust_iter: an iterable as returned from aclust.aclust()
-    clin_df: a pandas dataframe with the clinical data to be used in the model.
-             rows from clin_df should match columns in the data sent to
-             clust_iter.
-    """
+def gee_cluster(formula, methylation, covs, coef, cov_struct=Exchangeable(),
+        family=Gaussian()):
+    cov_rep = pd.concat((covs for i in range(len(methylation))))
+    nr, nc = methylation.shape
+    cov_rep['CpG'] = np.repeat(['CpG_%i' % i for i in range(methylation.shape[0])],
+                        methylation.shape[1])
+    cov_rep['methylation'] = np.concatenate(methylation)
 
-    yield "#chrom\tstart\tend\tn_probes\tprobes\t{coef}.pvalue\t{coef}.tstat\t{coef}.coef"\
+    res = GEE.from_formula(formula, groups=cov_rep['id'], data=cov_rep, cov_struct=cov_struct,
+            family=family).fit()
+    idx = [i for i, par in enumerate(res.model.exog_names)
+                       if par.startswith(coef)]
+    return {'p': res.pvalues[idx[0]],
+            't': res.tvalues[idx[0]],
+            'coef': res.params[idx[0]]}
+
+def stouffer_liptak(pvals, sigma):
+    pvals = np.array(pvals, dtype=np.float64)
+    pvals[pvals == 1] = 1.0 - 9e-16
+    qvals = norm.isf(pvals).reshape(len(pvals), 1)
+    C = np.asmatrix(chol(sigma)).I
+    qvals = C * qvals
+    Cp = qvals.sum() / np.sqrt(len(qvals))
+    return norm.sf(Cp)
+
+def liptak_cluster(formula, methylations, covs, coef, family=Gaussian()):
+
+    res = [GLM.from_formula(formula, covs, family=family).fit()
+            for methylation in methylations]
+
+    idx = [i for i, par in enumerate(res[0].model.exog_names)
+                       if par.startswith(coef)][0]
+    pvals = [r.pvalues[idx] for r in res]
+    return dict(t=np.mean([r.tvalues[idx] for r in res]),
+                coef=np.mean([r.params[idx] for r in res]),
+                p=stouffer_liptak(pvals, np.corrcoef(methylations)))
+
+
+def model_clusters(clust_iter, clin_df, model_str, coef, model_fn=gee_cluster):
+
+    yield "#chrom\tstart\tend\tn_probes\t{coef}.pvalue\t{coef}.tstat\t{coef}.coef\tprobes"\
             .format(**locals())
-
 
     for cluster in clust_iter:
 
         assert len(cluster[0].values)
 
-        methylation = np.concatenate([c.values for c in cluster])
-        locs = np.concatenate([[c.position] * len(c.values) for c in cluster])
-        df_rep = pd.concat((clin_df for i in range(len(cluster))))
-        """
-        # TODO: set this if autoregressive
-        locs = np.zeros_like(methylation)
-        for group in df.StudyID.unique():
-            jj = np.flatnonzero(df_rep.StudyID == group)
-            locs[jj] = range(len(jj))
-        """
-        res = GEE.from_formula(model_str, df_rep,
-                               family=family,
-                               #time=locs,
-                               groups=list(getattr(df_rep, id_col)),
-                               varstruct=varstruct).fit()
-
-        idx = [i for i, par in enumerate(res.model.exog_names)
-                       if par.startswith(coef)]
-        assert len(idx) == 1, (
-            "should have a single coefficent matching %s" % coef,
-            res.model.exog_names)
-        pval = res.pvalues[idx[0]]
-
-        yield "%s\t%i\t%i\t%i\t%s\t%.4g\t%.2f\t%.2f" % (
+        methylation = np.array([c.values for c in cluster])
+        r = model_fn(model_str, methylation, clin_df, coef)
+        yield "%s\t%i\t%i\t%i\t%.4g\t%.2f\t%.2f\t%s" % (
             cluster[0].chrom,
             cluster[0].position,
             cluster[-1].position,
             len(cluster),
-            ",".join(c.spos for c in cluster),
-            pval,
-            res.tvalues[idx[0]],
-            res.params[idx[0]])
-
+            r['p'], r['t'], r['coef'],
+            ",".join(c.spos for c in cluster))
 
 if __name__ == "__main__":
 
@@ -89,7 +96,7 @@ if __name__ == "__main__":
         __slots__ = "chrom position values spos".split()
 
         def __init__(self, chrom, pos, values):
-            self.chrom, self.position, self.values = chrom, pos, ilogit(np.array(values))
+            self.chrom, self.position, self.values = chrom, pos, np.array(values)
             self.spos = "%s:%i" % (chrom, pos)
 
         def distance(self, other):
@@ -112,16 +119,19 @@ if __name__ == "__main__":
             chrom, pos = toks[0].split(":")
             yield Feature(chrom, int(pos), map(float, toks[1:]))
 
-    df = pd.read_table('/home/brentp/src/denver-bio/2013/icac-nasal-epithelium/data/meth.clin.txt')
+    df = pd.read_table('meth.clin.txt')
+    df['id'] = df['StudyID']
     formula = "methylation ~ asthma + age + gender + race_white + race_hispanic + race_aa"
     clust_iter = (c for c in
-                    aclust(sorted(feature_gen()), max_dist=400, max_skip=2) if
+                    aclust(feature_gen(), max_dist=400, max_skip=2) if
                     len(c) > 1)
 
-    clusters = gee_cluster(clust_iter, df, formula, "asthma", "StudyID",
-                varstruct=Exchangeable(), family=Binomial())
+    clusters = model_clusters(clust_iter, df, formula, "asthma", model_fn=gee_cluster)
     from cruzdb import Genome
+    for i, c in enumerate(clusters):
+        print c
+        if i > 10: break
 
-    g = Genome('sqlite:///hg19.db')
-    g.annotate((x.split("\t") for x in clusters), ('refGene', 'cpgIslandExt'), feature_strand=True)
+    #g = Genome('sqlite:///hg19.db')
+    #g.annotate((x.split("\t") for x in clusters), ('refGene', 'cpgIslandExt'), feature_strand=True)
 
